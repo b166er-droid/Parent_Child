ffbeed333d3fb4997a0449066d231543e8315d13
b166er-droid <60444348+b166er-droid@users.noreply.github.com>
2021-03-26 08:54:05 -0300

    Update parent_child_pipe.c
    V2

--- parent_child_pipe.c
+++ parent_child_pipe.c
@@ -5,11 +5,13 @@
 int main(void)
 {
   pid_t pid;
-  int status;
-  int fd;
   int p[2];
-  char str[80];
-  int i;
+  FILE *fp;
+  int fd1, fd2;
+  //int status;
+  //int fd;
+  //char str[80];
+  //int i;
 
   pipe(p);
 
@@ -17,34 +19,46 @@ int main(void)
 
   if (pid == 0)
     {
-      dup2(p[0], 0);
-      close(p[0]);
+      char str[100];
+      
+      fp = fdopen(p[0], "r");
+      fd2 = creat("output2.txt", 0777);
+      dup2(fd2, 1);
 
       printf("\n\nThis is the child process.\n");
-      read(0, str, 16);
+      fgets(str, 100, fp);
       
-      while (strcmp(str, "\nHere is line XX") != 0)
+      while (strstr(str, "\nLast Line") == NULL)
 	{
 	  printf("\nReceived from parent process (through pipe): %s", str);
-	  read(0, str, 16);
+	  fflush(stdout);
+	  fgets(str, 100, fp);
 	}
+	printf("\nReceived the last line from the parent.");
     }
   else if (pid > 0)
     {
+    	int i;
+    	
       printf("\nThis is the parent process.");
-
-      for (i=0; i<10; i++)
+	fp = fdopen(p[1], "w");
+	fd1 = creat("output1.txt", 0777);
+        dup2(fd1, 1);
+	
+      for (i=0; i<100000; i++)
 	{
-	  strcpy(str, "Here is line #");
-	  str[14] = '0' + i;
-	  str[15] = '\0';
 
-	  printf("\nSending to child process (through pipe): %s", str);
-	  write(p[1], str, strlen(str)+1);
+	  printf("\nSending to child process (through pipe): Here is line %d", i);
+	  fprintf(fp, "Here is line %d\n", i);
 	}
 
-      strcpy(str, "Here is line XX");
-      printf("\nSending to child process (through pipe): %s", str);
-      write(p[1], str, strlen(str));
+      printf("\nSending the last line to child process (through pipe)");
+      fprintf(fp, "Last Line\n");
+      
+      fflush(fp);
     }
+    
+    close(fp);
+    close(p[0]);
+    close(p[1]);
 }
